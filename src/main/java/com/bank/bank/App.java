4f365778be99251948f8a8749f7d89be67416e34
RAS-BAVANI <rasbavani4@gmail.com>
2026-05-05 08:33:31 +0530

    Update App.java

--- src/main/java/com/bank/bank/App.java
+++ src/main/java/com/bank/bank/App.java
@@ -1,18 +1,25 @@
 package com.bank.bank;
-
+import java.util.ArrayList;
 public class App {
 	int balance=0;
   public int deposit(int amt) {
-	  if(amt <0) throw new IllegalArgumentException("amount not be zero");
+	  ArrayList<String> history=new ArrayList<>();
+	  if(amt <0) {
+		 
+		  throw new IllegalArgumentException("amount not be zero");
+	  }
 	  balance+=amt;
+	  history.add("deposit:"+ amt + " Balance:"+balance);
 	  return balance;
   }
   
   public int withdraw(int amt) {
 	  if(amt > balance) {
+		  history.add("Failed WithDraw:"+ amt + " Balance:"+balance);
 		  throw new ArithmeticException("amt should not go beyond balance");
 		  
 	  }
+	  history.add("WithDraw:"+ amt + " Balance:"+balance);
 	  balance-=amt;
 	  return balance;
   }
@@ -21,4 +28,8 @@ public class App {
   public int getBalance() {
 	  return balance;
   }
+
+	public List<String> getHistory(){
+		return history;
+	}
 }
